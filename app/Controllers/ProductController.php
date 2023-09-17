0beea627226c58a67120f92e8727cb4b01c5ad14
Dyzeeeeee <jdmalaluan2@gmail.com>
2023-09-18 01:19:17 +0800

    view

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -6,6 +6,16 @@ use App\Controllers\BaseController;
 
 class ProductController extends BaseController
 {
+
+    public function product($product)
+    {
+        return $product;
+    }
+
+    public function dyze()
+    {
+        return view('products');
+    }
     public function index()
     {
         //
