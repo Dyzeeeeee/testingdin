4dd3282d46250fcc79015f4dd5eef7f051fdfaad
Dyzeeeeee <jdmalaluan2@gmail.com>
2023-09-19 20:35:41 +0800

    All

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -12,17 +12,52 @@ class ProductController extends BaseController
         $this->product = new \App\Models\ProductModelo();
     }
 
+    public function edit($id)
+    {
+        $data = [
+        'product' => $this->product->findAll(),
+        'pro' => $this->product->where('id', $id)->first()
+    ];
+        return view('products', $data);
+        
+    }
+
     public function product($product)
     {
         return $product;
     }
 
+    public function delete($id)
+    {
+        $this->product->delete($id);
+        return redirect()->to('/product');
+    }
+
+    public function save()
+    {  
+        $id = $_POST['id'];
+        $data = [
+            'code' => $this->request->getVar('code'),
+            'name' => $this->request->getVar('name'),
+            'quantity' => $this->request->getVar('quantity'),
+         ];
+        if ($id != null){
+        $this->product->set($data)->where('id',$id)->update($data);
+            
+        }else{
+        $this->product->save($data);
+            
+        }
+      
+        return redirect()->to('/product');
+    }
     public function dyze()
     {
         // return view('products');
-        $data = $this->product->findAll();
-        echo '<pre>';
-        print_r($data);
+        $data['product'] = $this->product->findAll();
+        // echo '<pre>';
+        // print_r($data);
+        return view('products', $data);
     }
     public function index()
     {
--- app/Views/products.php
+++ app/Views/products.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <title>Products</title>
 </head>
 <body>
+    <form action="/save" method="post">
+        <label>code</label>
+        <input type="hidden" name="id" value="<?=$pro['id']?>">
+        <input type="text" name="code" placeholder="code" value="<?=$pro['code']?>">
+        <br>
+        <label>name</label>
+        <input type="text" name="name" placeholder="name" value="<?=$pro['name']?>">
+        <br>
+        <label>quantity</label>
+        <input type="text" name="quantity" placeholder="quantity" value="<?=$pro['quantity']?>">
+        <br>
+        <input type="submit" value="save">
+    </form>
     <h1>Product Listing</h1>
+    <table border = '1'>
+        <tr>
+            <th>code</th>
+            <th>name</th>
+            <th>quantity</th>
+            <th>action</th>
+            <?php foreach ($product as $pr):?>
+                <tr>
+                    <td><?=$pr['code']?></td>
+                    <td><?=$pr['name']?></td>
+                    <td><?=$pr['quantity']?></td>
+                    <td><a href="/delete/<?=$pr['id']?>">delete</a>||<a href="/edit/<?=$pr['id']?>">edit</a></td>
+                </tr>
+            <?php endforeach; ?>
+        </tr>
+    </table>
 </body>
 </html>
